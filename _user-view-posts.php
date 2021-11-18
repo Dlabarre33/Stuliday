8f5cd6ad7e0b04564e183f4e4fdf0d1c51f4678a
David L <dlabarre33@gmail.com>
2021-11-18 21:52:32 +0100

    Verification

--- _user-view-posts.php
+++ _user-view-posts.php
@@ -2,14 +2,17 @@
 
 require 'includes/connect.php';
 
-
-
-
-
-
-
-
+$user = $_GET['post_id'];
 if ($user) {
-
-    $post = $connexion->query('SELECT post FROM post')->fetchAll();
+    // $post = $connexion->query('SELECT * FROM post WHERE post_id')->fetchAll();
+    $userViewPost = "SELECT * from post WHERE post_id = :post_id";
+    $reqUserViewPost = $connexion->prepare($viewPost);
+    $reqUserViewPost->bindValue(':post_id', $id);
+    $reqUserViewPost->execute();
+    $post = $reqUserViewPost->fetch();
+    if (isset($post)) {
+        echo "Erreur";
+        echo '<meta http-equiv="refresh" content="0;URL=my-post.php?error=noId">';
+        exit();
+    }
 }
